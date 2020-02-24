d75964efe37ada8457ba07873663d37ad3bbbecd
Rohit Suthar <iamrohitsuthar@gmail.com>
2020-02-24 09:36:12 +0530

    PassTwoMacroProcessor Completed

--- PassTwoMacroProcessor/src/PassTwoMacroProcessor.java
+++ PassTwoMacroProcessor/src/PassTwoMacroProcessor.java
@@ -29,7 +29,7 @@ public class PassTwoMacroProcessor {
 		//read MNT
 		bufferedReader = new BufferedReader(new FileReader("MNT.txt"));
 		while((line = bufferedReader.readLine()) != null) {
-			Msg.println("MNT read: "+line);
+			//Msg.println("MNT read: "+line);
 			String params[] = line.split(" ");
 			MNT.put(params[1],new MacroDefModel(Integer.parseInt(params[2]), Integer.parseInt(params[3]), params[4], params[5]));
 		}
@@ -38,7 +38,7 @@ public class PassTwoMacroProcessor {
 		//read KPDTAB
 		bufferedReader = new BufferedReader(new FileReader("KPDTAB.txt"));
 		while((line = bufferedReader.readLine()) != null) {
-			Msg.println("KPDTAB read: "+line);
+			//Msg.println("KPDTAB read: "+line);
 			String params[] = line.split(" ");
 			KPDTAB.put(params[1], params[2]);
 		}
@@ -47,7 +47,7 @@ public class PassTwoMacroProcessor {
 		//read MDT
 		bufferedReader = new BufferedReader(new FileReader("MDT.txt"));
 		while((line = bufferedReader.readLine()) != null) {
-			Msg.println("MDT read: "+line);
+			//Msg.println("MDT read: "+line);
 			String params[] = line.split(" ");
 			if(params.length > 2)
 				MDT.add(params[1] + " " + params[2] + " " + params[3]);
@@ -70,22 +70,25 @@ public class PassTwoMacroProcessor {
 	}
 	
 	public void parse(BufferedReader bufferedReader) throws IOException {
+		String prev = null;
 		while((line = bufferedReader.readLine()) != null) {
 			String parts[] = line.split(" ");
 			if(MNT.containsKey(parts[0])) {
 				//macro call
 				APTAB.clear();
-				Msg.println("HERE: "+parts[0]);
 				
 				int pp = MNT.get(parts[0]).getPositionalParams();
 				int kp = MNT.get(parts[0]).getKeywordParams();
-				int kpdtabPointer = Integer.parseInt(MNT.get(parts[0]).getKeywordsDefPointer());
+				int kpdtabPointer = -1;
+				if(!MNT.get(parts[0]).getKeywordsDefPointer().equals("null"))
+					kpdtabPointer = Integer.parseInt(MNT.get(parts[0]).getKeywordsDefPointer());
+				
 				int total = pp + kp;
 				
 				int positionalPar = 1;
 				for(int i = 1 ; i <= pp ; i++) {
 					String extract = parts[positionalPar++].replaceAll("[,]+", "");
-					Msg.println("APTAB: "+extract);
+					//Msg.println("APTAB: "+extract);
 					APTAB.add(extract.trim());
 				}
 				int keywordPar = positionalPar;
@@ -93,29 +96,29 @@ public class PassTwoMacroProcessor {
 					if((parts.length-1) >= keywordPar) {
 						String extract = parts[keywordPar].replaceAll("[,]+", "");
 						extract = extract.substring(extract.indexOf("=")+1);
-						Msg.println("APTAB No Default: "+extract);
+						//Msg.println("APTAB No Default: "+extract);
 						APTAB.add(extract.trim());
 					}
 					else {
 						//default value
 						String temp = getFromHashMap(kpdtabPointer+j);
-						Msg.println("APTAB Default value: "+ temp);
+						//Msg.println("APTAB Default value: "+ temp);
 						APTAB.add(temp.trim());
 					}
 					keywordPar++;
 				}
-				
 				int mdtPointer = Integer.parseInt(MNT.get(parts[0]).getMacroDefPointer());
-				Msg.println(mdtPointer);
+//				if(prev != null && prev.equals("MEND"))
+//					mdtPointer++;
 				String ins = MDT.get(mdtPointer-1);
+				prev = ins;
+				Msg.println("HEL: "+ins);
 				while(!ins.equals("MEND")) {
 					Msg.println(ins);
 					String params[] = ins.split(" ");
 					bufferedWriter.write(params[0] + " ");
 					for(int i = 1; i < params.length; i++) {
 						String temp = params[i].replaceAll("[^0-9]", "");
-						//Msg.println("temp: "+temp);
-						//Msg.println(APTAB.get(Integer.parseInt(temp)));
 						if(i == params.length - 1)
 							bufferedWriter.write(APTAB.get(Integer.parseInt(temp)));
 						else
@@ -123,13 +126,14 @@ public class PassTwoMacroProcessor {
 					}
 					bufferedWriter.write("\n");
 					ins = MDT.get(++mdtPointer-1);
+					prev = ins;
+					Msg.println("HEL1: "+ins);
 				}
 			}
 			else {
 				bufferedWriter.write(line + "\n");
 			}
 		}
-		Msg.println("HERE LAST");
 		bufferedWriter.flush();
 		bufferedWriter.close();
 	}
