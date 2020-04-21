17940513177656d470936102b0d0558abf0909d1
iamrohitsuthar <iamrohitsuthar@gmail.com>
2020-04-21 15:45:14 +0530

    Fix pages variable declaration in FIFO Page replacement algo

--- PAGE_REPLACEMENT_ALGORITHMS/FIFO.java
+++ PAGE_REPLACEMENT_ALGORITHMS/FIFO.java
@@ -45,7 +45,7 @@ public class FIFO {
 	}
 
 	public static void main(String[] args) {
-		int capacity, n;
+		int capacity, n, pages[];
 		// int pages[] = {7,0,1,2,0,3,0,4,2,3,0,3,2,1,2,0,1,7,0,1};
 		FIFO fifo = new FIFO();
 
